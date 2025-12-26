c7fac06135099e31d88024f6524bb47369c061d1
hj <parkhj062@gmail.com>
2025-12-26 16:34:19 +0900

    [13. 문제와 풀이]
    - toString, equals implementation

---
+++ src/lang/object/test/Rectangle.java
@@ -0,0 +1,35 @@
+package lang.object.test;
+
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null){
+            return false;
+        }
+        if(obj instanceof Rectangle rectangle){
+            return width == rectangle.width
+                    && height == rectangle.height;
+        }
+
+        return false;
+
+    }
+
+
+}
