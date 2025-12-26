ca9547aa3bc3915ad0994fc42eae19f856ca2955
hj <parkhj062@gmail.com>
2025-12-26 19:27:22 +0900

    [24. String 클래스 - 불변 객체]
    - String class immutable. prevent side effects (string pool share same string instance)

---
+++ src/lang/string/immutable/StringImmutable1.java
@@ -0,0 +1,12 @@
+package lang.string.immutable;
+
+public class StringImmutable1 {
+    public static void main(String[] args) {
+        String str = "hello";
+        String str2 = str.concat("world");
+
+        System.out.println("str = " + str);
+        System.out.println("str2 = " + str2);
+    }
+}
+
