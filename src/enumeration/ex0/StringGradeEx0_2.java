fa98ec890e8ccddf6050494de07294fe6875e46b
hj <parkhj062@gmail.com>
2025-12-27 15:48:17 +0900

    [44. 문자열과 타입 안전성1]
    [problem]
    - unsafety with String
     - must allow exact value
     - compile time check impossible

---
+++ src/enumeration/ex0/StringGradeEx0_2.java
@@ -0,0 +1,16 @@
+package enumeration.ex0;
+
+public class StringGradeEx0_2 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount("basic", price);
+        int gold = discountService.discount("Gold", price);
+        int diamond = discountService.discount("DIAMONDD", price);
+
+        System.out.println("basic = " + basic);
+        System.out.println("gold = " + gold);
+        System.out.println("diamond = " + diamond);
+    }
+}
